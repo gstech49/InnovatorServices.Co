c819afe6b862bc41c710c2d2ec776eeb42d3ad5c
gstech49 <152642496+gstech49@users.noreply.github.com>
2024-05-12 10:20:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ dbConnection()
 //static files
 app.use(express.static(path.join(__dirname, '/dist')))
 app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 export default app
